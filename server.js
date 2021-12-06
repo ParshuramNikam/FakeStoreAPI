0ae296bd65cbe04ca17fb62a474e0a29ec7df9c5
Parshuram Nikam <parshunikam0612@gmail.com>
2021-12-06 20:56:45 +0530

    set production routes for client

--- server.js
+++ server.js
@@ -52,8 +52,26 @@ app.use(
 // for deployment :-
 if(process.env.NODE_ENV === "production"){
 	app.use(express.static('client/build'));
-	app.get('*',(req,res)=>{
+	app.get('/',(req,res)=>{
 		res.sendFile(path.join(path.resolve(),'client/build/server/pages', 'index.html'));
+	})
+	app.get('/dashboard', (req,res)=>{
+		res.sendFile(path.join(path.resolve(),'client/build/server/pages', 'dashboard.html'));
+	})
+	app.get('/docs', (req,res)=>{
+		res.sendFile(path.join(path.resolve(),'client/build/server/pages', 'docs.html'));
+	})
+	app.get('/user/forget-password', (req,res)=>{
+		res.sendFile(path.join(path.resolve(),'client/build/server/pages', 'Forget-password.html'));
+	})
+	app.get('/user/login', (req,res)=>{
+		res.sendFile(path.join(path.resolve(),'client/build/server/pages', 'Login.html'));
+	})
+	app.get('/user/signup', (req,res)=>{
+		res.sendFile(path.join(path.resolve(),'client/build/server/pages', 'Signup.html'));
+	})
+	app.get('/user/change-password/:otp/:userId', (req,res)=>{
+		res.sendFile(path.join(path.resolve(),'client/build/server/pages', 'Signup.html'));
 	})
 	
 }
